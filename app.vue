5730df3820bf7ad6aeb4d027bd34bd24074268be
alimpp <hmdmpp@gmail.com>
2025-02-14 11:02:25 -0800

    Add layout

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi numquam
+    corporis consectetur voluptatem, veniam architecto, maiores illo assumenda
+    cupiditate expedita commodi quaerat, reiciendis ratione soluta doloremque
+    quae eaque. Itaque, vero.
   </div>
 </template>
