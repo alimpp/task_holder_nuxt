06b02f5b276c777a7f08f639d7224844e7b18905
alimpp <hmdmpp@gmail.com>
2025-02-28 10:03:04 -0800

    Add utils in base app

--- stores/baseApp.ts
+++ stores/baseApp.ts
@@ -28,6 +28,29 @@ export class BaseAppModule {
       }
     );
   }
+
+  convertDataToJson(data: any) {
+    const response = JSON.stringify(data);
+    return response;
+  }
+
+  parseJsonData(data: any) {
+    const response = JSON.parse(data);
+    return response;
+  }
+
+  saveToLocaleStorage(key: string, data: any) {
+    localStorage.setItem(key, data);
+  }
+
+  getDataFromLocaleStorage(key: string) {
+    const response = localStorage.getItem(key);
+    return response;
+  }
+
+  removeItemFromLocaleStorage(key: string) {
+    localStorage.removeItem(key);
+  }
 }
 
 export const baseAppStoreModule = new BaseAppModule(false, []);
