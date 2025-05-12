5782c8adaefe5889d254058e3f6e252acb13fb1e
Alimp <A.Mohsenpour@sgas.ir>
2025-05-12 16:02:52 +0330

    Add skeleton loading

---
+++ components/base/EmptyState.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-column justify-center align-center w-100">
+      <img src="/public/image/empty-state-vector.png" alt="image" width="350" />
+      <span class="f-s-13 f-w-500 color-primary">{{ title }}</span>
+      <span class="f-s-11 f-w-500 color-gray pt-5">{{ text }}</span>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: {
+        type: String,
+        default: "No Data"
+    },
+    text: {
+        type: String,
+        default: "No Data"
+    }
+})
+</script>
--- pages/friends/send.vue
+++ pages/friends/send.vue
@@ -3,15 +3,23 @@
       <div class="w-100">
         <BaseBreadCrumbs />
       </div>
-      <div class="w-100 flex flex-column">
-        <CardsSendFriendRequest v-for="(item, index) in userList" :key="index" :user="item" />
+      <div class="flex w-100 h-80-dvh align-cemter justify-center" v-if="!pageLoading && userList.length == 0">
+        <BaseEmptyState title="Users Not Found" text="Users list is empty please try again later" />
       </div>
+     <div class="flex flex-column" v-if="pageLoading">
+      <SkeletonSendFriendRequest v-for="n in 10" />
+     </div>
+    <div class="w-100 flex flex-column" v-else>
+      <CardsSendFriendRequest v-for="(item, index) in userList" :key="index" :user="item" />
+    </div>
     </div>
 </template>
   
 <script setup>
 import { UsersStoreModule } from "~/stores/users";
 import { UsersControllerModule } from "~/controllers/users";
+
+const pageLoading = ref(false);
 
 const userList = computed(() => {
   return UsersStoreModule.userlist.value;
@@ -23,7 +31,9 @@ definePageMeta({
 });
 
 onMounted(async () => {
+  pageLoading.value = !pageLoading.value;
   await UsersControllerModule.getUsersList();
+  pageLoading.value = !pageLoading.value;
 });
 </script>
   
