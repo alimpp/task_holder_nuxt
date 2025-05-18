bd50e48ee5e2499d7a16fbbeabc1052e524761f9
Alimpp <86446834+alimpp@users.noreply.github.com>
2025-05-18 17:39:35 +0330

    Update downloadFile.get.ts

--- server/api/file/downloadFile.get.ts
+++ server/api/file/downloadFile.get.ts
@@ -1,14 +1,11 @@
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, "id");
-
   const headers = {
     "Content-Type": "application/json",
   };
-
   const response = await $fetch(`http://localhost:4000/files/${id}`, {
     method: "GET",
     headers: headers,
   });
-
   return response;
 });
