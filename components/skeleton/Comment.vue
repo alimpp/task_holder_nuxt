e6be4f18630aeed46debd0c15022c382caca4001
Alimp <A.Mohsenpour@sgas.ir>
2025-06-10 18:06:06 +0330

    Update ui

--- components/skeleton/Comment.vue
+++ components/skeleton/Comment.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex mt-5 slid-up-animation-1s">
+    <div class="flex mt-5 slid-up-animation-1s w-100">
         <BaseSkeleton width="35px" height="35px" radius="50%" class="mt-5" />
-        <div class="flex flex-column py-5 px-5 border-bottom-gray w-320-px">
+        <div class="flex flex-column py-5 px-5 border-bottom-gray w-100">
             <BaseSkeleton width="100px" height="15px" />
             <BaseSkeleton width="150px" height="15px" class="mt-7" />
         </div>
